6ef12faea4110d75934d4f03062a0db3d0a2dc98
Bisrat <jenberehenok@gmail.com>
2025-09-04 18:19:31 +0300

    added

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'yoga-layout': 'yoga-layout-prebuilt'
-    }
-  },
-  define: {
-    'require': undefined
-  },
   optimizeDeps: {
-    exclude: ['yoga-layout'] // avoid pre-bundle error
-  }
-})
+    include: ['react/jsx-runtime', 'react/jsx-dev-runtime'],
+  },
+});
